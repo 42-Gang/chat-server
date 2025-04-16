e931c8741c0096a0423c7276f45c825ff49b91c1
Jeongsu <latsyrc900@gmail.com>
2025-04-16 14:24:31 +0900

    refactor: handleIncomingMessage 함수 시그니처 변경

--- src/v1/sockets/chat/connection.handler.ts
+++ src/v1/sockets/chat/connection.handler.ts
@@ -31,36 +31,36 @@ type HandleIncomingMessageParams = {
   payload: unknown;
 };
 
-async function handleIncomingMessage(data: HandleIncomingMessageParams) {
+async function handleIncomingMessage({socket, chatService, userId, payload}: HandleIncomingMessageParams) {
   try {
-    const { messageData, roomType, otherUserId } = await validateIncomingMessage(data);
+    const { messageData, roomType, otherUserId } = await validateIncomingMessage(userId, payload);
 
-    data.socket.to(`room:${messageData.roomId}`).emit('message', messageData);
+    socket.to(`room:${messageData.roomId}`).emit('message', messageData);
 
     if (roomType === ChatRoomType.PRIVATE && otherUserId !== undefined) {
-      const isBlocked = await checkBlockStatus(otherUserId, data.userId);
+      const isBlocked = await checkBlockStatus(otherUserId, userId);
       if (isBlocked) return;
     }
 
-    await data.chatService.saveMessage(messageData);
+    await chatService.saveMessage(messageData);
     // TODO: Kafka로 메시지 전송
 
   } catch (e) {
     console.error('❌ 메시지 처리 실패:', e);
-    data.socket.emit('error_message', { message: '메시지 포맷 오류 또는 권한 문제' });
+    socket.emit('error_message', { message: '메시지 포맷 오류 또는 권한 문제' });
   }
 }
 
-async function validateIncomingMessage(data: HandleIncomingMessageParams): Promise<{
+async function validateIncomingMessage(userId: number, payload: unknown): Promise<{
   messageData: ResponseMessage;
   roomType: ChatRoomType;
   otherUserId?: number;
 }> {
-  const { roomId, contents } = requestMessageSchema.parse(data.payload);
+  const { roomId, contents } = requestMessageSchema.parse(payload);
 
   const messageData: ResponseMessage = responseMessageSchema.parse({
     roomId,
-    userId: data.userId,
+    userId: userId,
     contents,
     time: new Date().toISOString(),
   });
@@ -70,14 +70,14 @@ async function validateIncomingMessage(data: HandleIncomingMessageParams): Promi
     dependencies.chatJoinListRepository.findManyByRoomId(roomId),
   ]);
 
-  const isUserInRoom = members.some((member) => member.userId === data.userId);
+  const isUserInRoom = members.some((member) => member.userId === userId);
   if (!isUserInRoom) {
     throw new ForbiddenException('이 채팅방에 참여하지 않은 사용자입니다');
   }
 
   let otherUserId: number | undefined = undefined;
   if (roomType === ChatRoomType.PRIVATE) {
-    otherUserId = members.find((join) => join.userId !== data.userId)?.userId;
+    otherUserId = members.find((join) => join.userId !== userId)?.userId;
     if (!otherUserId) {
       throw new Error('상대방을 찾을 수 없습니다 (1:1 채팅방 아님)');
     }
