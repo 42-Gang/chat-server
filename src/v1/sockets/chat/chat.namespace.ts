1b41b02baa3c0311656e876f7b16d828d404f448
Jeongsu <107098751+JeongSu0880@users.noreply.github.com>
2025-04-16 21:36:08 +0900

    fix: sockets/chat.service.ts -> socket/chat.manager.ts로 변경하여 dicontainer 주입 시 혼동 해결 (#8)

--- src/v1/sockets/chat/chat.namespace.ts
+++ src/v1/sockets/chat/chat.namespace.ts
@@ -1,11 +1,11 @@
 import { Namespace } from 'socket.io';
 import { socketMiddleware } from '../utils/middleware.js';
 import { handleConnection } from './connection.handler.js';
-import ChatService from './chat.service.js';
+import ChatManager from './chat.manager.js';
 
 export default async function chatNamespace(namespace: Namespace) {
   namespace.use(socketMiddleware);
 
-  const chatService = new ChatService();
-  namespace.on('connection', (socket) => handleConnection(socket, chatService));
+  const chatManager = new ChatManager();
+  namespace.on('connection', (socket) => handleConnection(socket, chatManager));
 }
