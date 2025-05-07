c3ff7f6efff2c3a4c7585b1779e02f45a1ad0c7a
Jeongsu0880 <latsyrc900@gmail.com>
2025-05-07 21:58:42 +0900

    feature: 채팅방 id 조회 get api 추가

--- src/v1/apis/chat.controller.ts
+++ src/v1/apis/chat.controller.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import ChatService from './chat.service.js';
 import { getMessagesParamsSchema } from './schemas/get-messages.schema.js';
+import { getDmRoomIdQuerySchema } from './schemas/get-room-id.schema.js';
 
 export default class ChatController {
   constructor(private readonly chatService: ChatService) {}
@@ -10,4 +11,10 @@ export default class ChatController {
     const result = await this.chatService.loadMessages(params.roomId, request.userId);
     reply.code(200).send(result);
   };
+
+  getRoomId = async (request: FastifyRequest, reply: FastifyReply) => {
+    const parsed = getDmRoomIdQuerySchema.parse(request.query);
+    const result = await this.chatService.getRoomId(parsed);
+    reply.status(200).send(result);
+  }
 }
