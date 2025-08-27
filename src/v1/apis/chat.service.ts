97939d312b7ac1e1838fd18820881b5a8c548be8
Woongbi Shin <dobby12335@gmail.com>
2025-08-27 12:19:35 +0900

    [FIX] 채팅 메시지 로딩 시 N+1 문제 해결 (#37)
    * feat(chat.service.ts, chatRoom.repository.ts): improve message loading by fetching user nicknames and handling errors
    
    * refactor(chat.service.ts): remove unused messagesToResponse function
    
    * fix(chat.service.ts): optimize message loading by using room members data

--- src/v1/apis/chat.service.ts
+++ src/v1/apis/chat.service.ts
@@ -1,37 +1,26 @@
-import ChatRoomRepositoryInterface from '../storage/database/interfaces/chatRoom.repository.interface.js';
-import ChatJoinListRepositoryInterface from '../storage/database/prisma/chatJoinList.repository.js';
 import ChatMessageRepositoryInterface from '../storage/database/prisma/chatMessage.repository.js';
 
-import { ChatMessage } from '@prisma/client';
 import { STATUS } from '../common/constants/status.js';
 import { NotFoundException, UnAuthorizedException } from '../common/exceptions/core.error.js';
 import { TypeOf } from 'zod';
 import { getDmRoomIdQuerySchema } from './schemas/get-room-id.schema.js';
 import { getUserNick } from '../sockets/chat/chat.client.js';
+import { FastifyBaseLogger } from 'fastify';
+import ChatRoomRepositoryPrisma from '../storage/database/prisma/chatRoom.repository.js';
 
 export default class ChatService {
   constructor(
-    private readonly chatJoinListRepository: ChatJoinListRepositoryInterface,
     private readonly chatMessageRepository: ChatMessageRepositoryInterface,
-    private readonly chatRoomRepository: ChatRoomRepositoryInterface,
+    private readonly chatRoomRepository: ChatRoomRepositoryPrisma,
+    private readonly logger: FastifyBaseLogger,
   ) {}
 
-  private async messagesToResponse(messages: ChatMessage) {
-    const nickname = await getUserNick(messages.userId);
-    return {
-      id: messages.id,
-      nickname: nickname,
-      timestamp: new Date(messages.timestamp),
-      message: messages.contents,
-    };
-  }
-
   async loadMessages(roomId: number, userId: number | undefined) {
-    const rooms = await this.chatRoomRepository.findById(roomId);
-    if (!rooms) throw new NotFoundException('채팅방이 존재하지 않습니다.');
+    const roomWithMembers = await this.chatRoomRepository.findByIdJoinMembers(roomId);
+    if (!roomWithMembers) throw new NotFoundException('채팅방이 존재하지 않습니다.');
 
-    const members = await this.chatJoinListRepository.findManyByRoomId(roomId);
-    if (!members || !members.some((user) => user.userId === userId)) {
+    const chatJoinList = roomWithMembers.members;
+    if (!chatJoinList || !chatJoinList.some((user) => user.userId === userId)) {
       throw new UnAuthorizedException('사용자가 포함된 채팅방이 아닙니다.');
     }
 
@@ -40,7 +29,27 @@ export default class ChatService {
       throw new NotFoundException('채팅 메시지가 존재하지 않습니다.');
     }
 
-    const response = await Promise.all(messages.map((item) => this.messagesToResponse(item)));
+    const members: Record<number, string> = Object.fromEntries(
+      await Promise.all(
+        chatJoinList.map(async (item): Promise<[number, string]> => {
+          const nickname = await getUserNick(item.userId);
+          if (!nickname) {
+            this.logger.error(`Failed to fetch nickname for userId: ${item.userId}`);
+            return [item.userId, 'unknown'];
+          }
+          return [item.userId, nickname];
+        }),
+      ),
+    );
+
+    const response = messages.map((message) => {
+      return {
+        id: message.id,
+        nickname: members[message.userId],
+        timestamp: new Date(message.timestamp),
+        message: message.contents,
+      };
+    });
 
     return {
       status: STATUS.SUCCESS,
